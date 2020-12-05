1314bb20247132567fc1d6f44d0f7f8ff8c88012
Weidong Xu <weidongxu84@hotmail.com>
2020-12-05 09:45:50 +0800

    update dependency

--- src/test/java/io/weidongxu/webapp/textnormalization/TextNormalizationApplicationTests.java
+++ src/test/java/io/weidongxu/webapp/textnormalization/TextNormalizationApplicationTests.java
@@ -1,11 +1,13 @@
 package io.weidongxu.webapp.textnormalization;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@Disabled
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TextNormalizationApplicationTests {
 
